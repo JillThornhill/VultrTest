c6e97f471b08493142d123c483ca65758fc0f51c
JillThornhill <jill.thornhill@zol.co.zw>
2023-07-03 13:46:34 +0200

    Removed nixos-anywhere from inputs & ran nix flake lock

--- flake.nix
+++ flake.nix
@@ -4,10 +4,8 @@
   inputs.disko.url = github:nix-community/disko;
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.nixos-anywhere.url = "github:numtide/nixos-anywhere";
-  inputs.nixos-anywhere.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, disko, nixos-anywhere, ... }@attrs: {
+  outputs = { self, nixpkgs, disko, ... }@attrs: {
     nixosConfigurations.vultr = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
